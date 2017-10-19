60ff5672f6ff465db1055ff927be52eeb0d56b62
thorsten-l <t.ludewig@ostfalia.de>
2017-10-19 12:01:39 +0200

    renaming project

--- src/main/java/sonia/rocketchat/client/RocketChatRestClientFactory.java
+++ src/main/java/sonia/rocketchat/client/RocketChatRestClientFactory.java
@@ -1,6 +1,7 @@
 package sonia.rocketchat.client;
 
 //~--- JDK imports ------------------------------------------------------------
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -9,56 +10,55 @@ import javax.ws.rs.client.WebTarget;
  *
  * @author th
  */
-public class RocketChatRestClientFactory {
+public class RocketChatRestClientFactory
+{
 
-    /**
-     * Field description
-     */
-    private final static String DEFAULT_API_PATH
-            = "/api/v1";
+  /**
+   * Field description
+   */
+  private final static String DEFAULT_API_PATH = "/api/v1";
 
-    //~--- constructors ---------------------------------------------------------
-    /**
-     * Constructs ...
-     *
-     */
-    private RocketChatRestClientFactory() {
-    }
+  //~--- constructors ---------------------------------------------------------
 
-    //~--- methods --------------------------------------------------------------
-    /**
-     * Method description
-     *
-     *
-     * @param apiUrl
-     * @param user
-     * @param password
-     *
-     * @return
-     */
-    public static RocketChatRestClient createClient(String apiUrl) {
-        Client client
-                = ClientBuilder.newClient();
+  /**
+   * Constructs ...
+   *
+   */
+  private RocketChatRestClientFactory() {}
 
-        WebTarget target = client.target(apiUrl);
+  //~--- methods --------------------------------------------------------------
 
-        return new RocketChatRestClient(target);
-    }
+  /**
+   * Method description
+   *
+   *
+   * @param apiUrl
+   *
+   * @return
+   */
+  public static RocketChatRestClient createClient(String apiUrl)
+  {
+    Client client = ClientBuilder.newClient();
 
-    /**
-     * Method description
-     *
-     *
-     * @param hostname
-     * @param secure
-     * @param user
-     * @param password
-     *
-     * @return
-     */
-    public static RocketChatRestClient createClient(String hostname, boolean secure ) {
-        return createClient(((secure)
-                ? "https"
-                : "http") + "://" + hostname + DEFAULT_API_PATH);
-    }
+    WebTarget target = client.target(apiUrl);
+
+    return new RocketChatRestClient(target);
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param hostname
+   * @param secure
+   *
+   * @return
+   */
+  public static RocketChatRestClient createClient(String hostname,
+    boolean secure)
+  {
+    return createClient(((secure)
+      ? "https"
+      : "http") + "://" + hostname + DEFAULT_API_PATH);
+  }
 }
